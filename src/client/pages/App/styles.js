b03b5217b521a65cf1ee449645d84437088fca7c
Lcharvol <charvolin0@gmail.com>
2018-09-22 21:30:19 +0200

    WIP

--- src/client/pages/App/styles.js
+++ src/client/pages/App/styles.js
@@ -2,7 +2,13 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     display:flex;
+    justify-content: center;
+    align-items: center;
     width:100vw;
     min-height:100vh;
     background-color:rgb(25,25,25);
+    font-family: 'Raleway', sans-serif;
+    color:white;
+    font-weight:0;
+    font-size:3em;
 `;
