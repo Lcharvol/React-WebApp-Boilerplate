cc4d5f9d1dc29eaa1d98375fca8578330ab7df09
Lcharvol <charvolin0@gmail.com>
2018-09-22 21:24:01 +0200

    Add react redux

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import configureStore from './store';
 import App from './pages/App';
 
+const initialState = {};
+export const store = configureStore(initialState);
+
 const Root = () => (
-    <App/>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
 ReactDOM.render(<Root />, document.getElementById('__NEWPROJECT__'));
